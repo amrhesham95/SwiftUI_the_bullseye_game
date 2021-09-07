6692e6f79bc04561eaca815e58a0da5449f2450f
amrhesham95 <47639999+amrhesham95@users.noreply.github.com>
2021-09-07 10:54:31 +0200

    initial commit

---
+++ BullsEye/BullsEye/BullsEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullsEyeApp.swift
+//  BullsEye
+//
+//  Created by Amr Hesham on 07/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct BullsEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
